0bfed0d16a8db783b3979aac118f5826dc2f19da
benmoorechd <58787861+benmoorechd@users.noreply.github.com>
2020-06-10 14:54:48 -0500

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -5,13 +5,13 @@ import scrapy
 class ToScrapeCSSSpider(scrapy.Spider):
     name = "toscrape-css"
     start_urls = [
-        'http://quotes.toscrape.com/',
+        'https://www.doralkia.com/searchnew.aspx?pn=50',
     ]
 
     def parse(self, response):
         for quote in response.css("div.quote"):
             yield {
-                'text': quote.css("span.text::text").extract_first(),
+                'text': quote.css("span.notranslate::text").extract_first(),
                 'author': quote.css("small.author::text").extract_first(),
                 'tags': quote.css("div.tags > a.tag::text").extract()
             }
