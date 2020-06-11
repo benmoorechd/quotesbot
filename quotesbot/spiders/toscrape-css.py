1392730fcd76448408cefec28910e7c912b36bc5
benmoorechd <58787861+benmoorechd@users.noreply.github.com>
2020-06-11 09:54:09 -0500

    Update toscrape-css.py

--- quotesbot/spiders/toscrape-css.py
+++ quotesbot/spiders/toscrape-css.py
@@ -3,20 +3,108 @@ import scrapy
 
 
 class ToScrapeCSSSpider(scrapy.Spider):
-    name = "toscrape-css"
-    start_urls = [
-        'https://www.doralkia.com/searchnew.aspx',
+name = "www.doralkia.com"
+    allowed_domains = ['www.doralkia.com']
+    start_urls = ['https://www.doralkia.com/searchnew.aspx']
+    rules = [
+        Rule(
+            LinkExtractor(
+                allow=('.*'),
+                deny=()
+            ),
+            callback='parse_item',
+            follow=True
+        )
     ]
-
-    def parse(self, response):
-        for quote in response.css("div.quote"):
-            yield {
-                'text': quote.css("span.notranslate::text").extract_first(),
-                'author': quote.css("small.author::text").extract_first(),
-                'tags': quote.css("div.tags > a.tag::text").extract()
-            }
-
-        next_page_url = response.css("li.next > a::attr(href)").extract_first()
-        if next_page_url is not None:
-            yield scrapy.Request(response.urljoin(next_page_url))
-
+    items = [
+        [
+            Item(
+                CarsForSaleMiamiFlDoralKia1Item,
+                None,
+                '.col-md-9',
+                [
+                    Field(
+                        'VIN',
+                        'div:nth-child(4)::attr(data-vin)',
+                        []),
+                    Field(
+                        'Condition',
+                        'div:nth-child(4)::attr(data-vehicletype)',
+                        []),
+                    Field(
+                        'Trim',
+                        'div:nth-child(4)::attr(data-trim)',
+                        []),
+                    Field(
+                        'Transmission',
+                        'div:nth-child(4)::attr(data-trans)',
+                        []),
+                    Field(
+                        'Stock',
+                        'div:nth-child(4)::attr(data-stocknum)',
+                        []),
+                    Field(
+                        'Sale_Price',
+                        'div:nth-child(4)::attr(data-price)',
+                        []),
+                    Field(
+                        'MSRP',
+                        'div:nth-child(4)::attr(data-msrp)',
+                        []),
+                    Field(
+                        'Bodystyle',
+                        'div:nth-child(4)::attr(data-bodystyle)',
+                        []),
+                    Field(
+                        'Certified',
+                        'div:nth-child(4)::attr(data-cpo)',
+                        []),
+                    Field(
+                        'Engine',
+                        'div:nth-child(4)::attr(data-engine)',
+                        []),
+                    Field(
+                        'Exterior_Color',
+                        'div:nth-child(4)::attr(data-extcolor)',
+                        []),
+                    Field(
+                        'Fuel_Type',
+                        'div:nth-child(4)::attr(data-fueltype)',
+                        []),
+                    Field(
+                        'Interior_Color',
+                        'div:nth-child(4)::attr(data-intcolor)',
+                        []),
+                    Field(
+                        'Make',
+                        'div:nth-child(4)::attr(data-make)',
+                        []),
+                    Field(
+                        'Model',
+                        'div:nth-child(4)::attr(data-model)',
+                        []),
+                    Field(
+                        'Model_Code',
+                        'div:nth-child(4)::attr(data-modelcode)',
+                        []),
+                    Field(
+                        'MPG_City',
+                        'div:nth-child(4)::attr(data-mpgcity)',
+                        []),
+                    Field(
+                        'MPG_Highway',
+                        'div:nth-child(4)::attr(data-mpghwy)',
+                        []),
+                    Field(
+                        'Full_Title',
+                        'div:nth-child(4)::attr(data-name)',
+                        []),
+                    Field(
+                        'Year',
+                        'div#srpVehicle-KNDEUCAA0M7050784::attr(data-year)',
+                        []),
+                    Field(
+                        'Image',
+                        'div:nth-child(4) > .col-md-12 > .well > .well > .row > div:nth-child(1) > .vehiclePhoto > .img-responsive::attr(data-img)',
+                        [],
+                        True)])]]
